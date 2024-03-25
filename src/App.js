e21f33114391190c1d32236045ba10c7c028fba9
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2024-03-25 20:55:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Posts Sharer</h1>
+        <h1>React Posts!</h1>
       </header>
     </div>
   );
